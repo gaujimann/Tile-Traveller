b61e9c7d0aef51b177ba0004a0fa86ca44c4a07e
hjalti <hjalti19@ru.is>
2019-09-17 10:52:14 +0000

    Code for moving around compete

--- tile_traveller.py
+++ tile_traveller.py
@@ -3,4 +3,30 @@
 #Write a code to check your input
 #Run input through the movement function
 #Write code for victiory condition
-#
+
+def dir(x,y,i):
+    if i == "n":
+        y += 1
+        return(y)
+    elif i == "s":
+        y -= 1
+        return(y)
+    elif i == "e":
+        x += 1
+        return(x)
+    elif i == "w":
+        x -= 1
+        return(x)
+x = 1
+y = 1
+while True:
+    move = input("Choose direction: ")
+    d = dir(x,y,move)
+    if move == "n" or move == "s":
+        print(x,d)
+        y = dir(x,y,move)
+    elif move == "e" or move == "w":
+        print(d,y)
+        x = dir(x,y,move)
+    
+    
