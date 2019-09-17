4e906ebfc37ceb7d3f1bb927b502cc109f35e3cc
Guðjón Guðjónsson <gudjong19@ru.is>
2019-09-17 09:35:54 +0000

    Algorithm for tile_traveler.py

---
+++ tile_traveller.py
@@ -0,0 +1,5 @@
+#Make function to tell you the valid direction
+#Make function to let you pick a direction
+#Write a code to check your input
+#Run input through the movement function
+#Write code for victiory condition
